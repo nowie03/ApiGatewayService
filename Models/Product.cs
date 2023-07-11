d3c8280faf4a5268f859f616d49bf2b7579d0a16
Immanuel <nowpertimmanuel@gmail.com>
2023-07-12 00:34:55 +0530

    fix : fixed ProductGetResponseModel

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,21 +1,26 @@
-﻿namespace ApiGatewayService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiGatewayService.Models
 {
     public class Product
     {
 
+
+        [Key]
         public int Id { get; set; }
 
-
+        [Required]
         public int CategoryId { get; set; }
 
+        [Required]
+        public string Name { get; set; }
 
-
+        [Required]
         public double Price { get; set; }
-
 
         public string Description { get; set; }
 
-
+        [Required]
         public string Address { get; set; }
 
         public DateTime CreatedAt { get; set; }
--- ResponseModels/ProductGetResponseModel.cs
+++ ResponseModels/ProductGetResponseModel.cs
@@ -5,23 +5,31 @@ namespace ApiGatewayService.ResponseModels
 {
     public class ProductGetResponse
     {
+       
+       
         public int Id { get; set; }
 
-
+       
         public Category Category { get; set; }
 
+       
+        public string Name { get; set; }
 
+       
         public double Price { get; set; }
-
 
         public string Description { get; set; }
 
+       
         public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
 
         public IEnumerable<ProductImage> Images { get; set; }
 
 
-        public ProductGetResponse(int id, Category category, double price, string description, string address, IEnumerable<ProductImage> images)
+        public ProductGetResponse(int id, Category category, double price, string description, string address,string name, IEnumerable<ProductImage> images)
         {
             Id = id;
             Category = category;
@@ -29,6 +37,7 @@ namespace ApiGatewayService.ResponseModels
             Description = description;
             Address = address;
             Images = images;
+            Name = name;
         }
     }
 }
