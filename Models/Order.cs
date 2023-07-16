40754626ff0347a6204dc10310095a13af5eac2a
Immanuel <nowpertimmanuel@gmail.com>
2023-07-17 02:03:38 +0530

    fix:fixed order post method

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,7 +8,7 @@ namespace ApiGatewayService.Models
         public int Id { get; set; }
 
         [Required]
-        public int ProductId { get; set; }
+        public int ProdudtId { get; set; }
 
         [Required]
         public int Quantity { get; set; }
